7734f82a634c7f13910f3399c3715fc2816d4ae8
Rihab <94738556+Rihab-S@users.noreply.github.com>
2025-04-06 12:03:06 +0200

    Create project.js

---
+++ project.js
@@ -0,0 +1,15 @@
+<script>
+  const slideShows = document.querySelectorAll('.slideshow-container');
+
+  slideShows.forEach(container => {
+    let index = 0;
+    const images = container.querySelectorAll('.slideshow-image');
+    images.forEach((img, i) => img.style.display = i === 0 ? 'block' : 'none');
+
+    setInterval(() => {
+      images[index].style.display = 'none';
+      index = (index + 1) % images.length;
+      images[index].style.display = 'block';
+    }, 1000);
+  });
+</script>
